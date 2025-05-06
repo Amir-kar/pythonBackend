3c60f26b6e25330fc00ae39765381eb2a44720f3
Amir Karpov <141690906+Amir-kar@users.noreply.github.com>
2025-05-06 08:48:18 -0400

    Add files via upload

---
+++ API/login.py
@@ -0,0 +1,28 @@
+import jwt
+
+from Token import token
+from flask import request, jsonify, Blueprint
+from flask_restful import Resource
+
+api = Blueprint('test', __name__)
+
+
+
+@api.route('/Hello', methods=['POST'])
+@token.token_required
+def getSome(id=0):
+    print(f"{id} requester")
+    return {"talkback": "Hello There"}
+# class BackendAuth:
+#     def __init__(self,app):
+#         self.app = app
+#     @self.app
+#     def login(self):
+#         return "",200
+#
+
+@api.route("/getToken")
+def post(id=0):
+    retToken = token.encode("User",5)
+    return retToken, 200
+#
---
+++ db/testFiles.py
@@ -0,0 +1,25 @@
+from flask import request, jsonify
+from flask_restful import Resource
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://db:27017")
+db = client.TestDB
+testingDB = db["Tests"]
+
+class createTest(Resource):
+    def post(self):
+        testingDB.insert_one({
+            "Hello":1,
+            "Bye":"Test"
+        })
+        retJSon = {
+            "status": 201,
+            "msg": "Post in another file created"
+        }
+        return jsonify(retJSon)
+    def get(self):
+        retJson = {
+            "status": 200,
+            "msg": testingDB.find({"Hello":1})[0]["Bye"]
+        }
+        return jsonify(retJson)
